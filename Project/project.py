9d1ec17203aa58a31d300bd7d2ec893d47bcd9a7
Coolroo <coolrooswag@gmail.com>
2021-10-08 20:19:08 -0300

    modified:   Project/project.py

--- Project/project.py
+++ Project/project.py
@@ -83,7 +83,22 @@ choosingColor = False
 connection = RawConnection()
 
 #GPIOGarbage
-#Setup for the GPIO Board
+"""
+Sets up the GPIO pins for the LED display.
+
+Parameters
+----------
+dataPin : int
+    The pin that the data line of the display is connected to.
+clockPin : int
+    The pin that the clock line of the display is connected to.
+latchpins : list of int
+    The pins that the latch line of the display is connected to.
+
+Returns
+-------
+None
+"""
 def setup():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(dataPin, GPIO.OUT)
@@ -91,7 +106,12 @@ def setup():
     for pin in latchpins:
         GPIO.setup(pin, GPIO.OUT)
 
-#Cleans up the GPIO board
+"""
+    This function is used to destroy the GPIO setup.
+    It is called when the program is exited.
+    It takes no parameters.
+    It returns nothing.
+"""
 def destroy():
     GPIO.cleanup()
 
@@ -99,7 +119,13 @@ def destroy():
 
 #Util
 
-#Resets the game to the state it was in before the game started
+"""
+    This function resets the game state by setting all the shift registers to 0 and setting the game state to the initial blank state.
+    Parameters:
+        None
+    Returns:
+        None
+"""
 def resetGameState():
     global gameInProgress
     global gameState
@@ -120,7 +146,16 @@ def resetGameState():
         GPIO.output(pin, GPIO.HIGH)
         GPIO.output(pin, GPIO.LOW)
 	
-#Sets the color of a given player, as long as it is not the marker color and not the other player's color
+"""
+    Changes the color of the player with the given ID to the given color.
+    
+    Parameters:
+        playerID (int): The ID of the player to change the color of.
+        newColor (list): The new color to change the player to.
+        
+    Returns:
+        None
+"""
 def chooseColor(playerID, newColor): 
     if(not (playerID == PLAYER2_NUM or playerID == PLAYER1_NUM)):
         print("Recieved an invalid player ID: " + str(playerID))
@@ -130,12 +165,33 @@ def chooseColor(playerID, newColor):
         playerColors[0 if playerID == PLAYER1_NUM else 1] = newColor
         print("Successfully changed player " + str(playerID) + "'s color")
 
-#Clears the board
+"""
+This function clears the game board, 
+and sets the game state to the default state.
+
+Parameters:
+    None
+
+Returns:
+    None
+"""
 def clearBoard():
     global gameState
     gameState = [[BLANK_NUM,BLANK_NUM,BLANK_NUM],[BLANK_NUM,BLANK_NUM,BLANK_NUM],[BLANK_NUM,BLANK_NUM,BLANK_NUM]]
 
-#Checks to see if any player has won
+"""
+Checks the game state for a win condition.
+
+Parameters
+----------
+gameState : list
+    A list of lists representing the game state.
+
+Returns
+-------
+bool
+    True if a win condition exists, False otherwise.
+"""
 def checkForWin():
     #Check Rows
     for row in gameState:
@@ -164,6 +220,15 @@ def checkForWin():
             return True
     return False
 
+"""
+Checks if the game is over by checking if there are any unnblocked rows.
+
+Parameters:
+    gameState (list): A list of lists representing the game state.
+
+Returns:
+    bool: True if the game is over, False otherwise.
+"""
 def checkForFail():
     cols = [[], [], []]
     diag = [[], []]
@@ -187,7 +252,16 @@ def checkForFail():
     fail()
     return True
 
-#Win as a given player
+"""
+This function is used to declare a winner of the game. 
+It takes a player number and displays the winning animation.
+
+Parameters:
+    playerNum (int): The player number that won.
+
+Returns:
+    None
+"""
 def won(playerNum):
     global gameInProgress
     print("Player " + str(playerNum) + " won!")
@@ -205,6 +279,16 @@ def won(playerNum):
     time.sleep(1)
     gameInProgress = False
 
+"""
+This function is used to indicate that the game has failed. 
+It will do this by flashing the board red three times.
+
+Parameters:
+    None
+
+Output:
+    None
+"""
 def fail():
     global gameInProgress
     shiftOut(buildColorStates(BOARD_X, {"1" : [True, False, False]}))
@@ -223,7 +307,18 @@ def fail():
         
 
 #Gameplay
-#Starts the game
+"""
+startGame()
+    Starts a game of Tic Tac Toe.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+"""
 def startGame():
     global prevStartState
     global gameInProgress
@@ -239,7 +334,15 @@ def startGame():
             break
     resetGameState()
 
-#Makes a move based on the current player and the marker position
+"""
+Makes a move for the current player at the current marker position.
+
+Parameters:
+    None
+
+Returns:
+    True if the move was made, False otherwise
+"""
 def makeMove():
     global turnState
     if gameState[markerPos[0]][markerPos[1]] == BLANK_NUM:
@@ -256,21 +359,58 @@ def makeMove():
 
 #Interaction
 
-#Moves the marker based on its relative position
+"""
+This function moves the marker a relative distance from its current position.
+
+Parameters
+----------
+x : int
+    The number of squares to move the marker horizontally.
+x must be a positive integer.
+y : int
+    The number of squares to move the marker vertically.
+y must be a positive integer.
+
+Returns
+-------
+None
+
+"""
 def moveMarkerRelative(x, y):
     markerPos[1] += x
     markerPos[0] += y
     markerPos[1] %= 3
     markerPos[0] %= 3
 
-#Sets the marker's absolute position
+"""
+Sets the position of the marker on the grid.
+
+Parameters
+----------
+x : int
+    The x coordinate of the marker.
+y : int
+    The y coordinate of the marker.
+
+Returns
+-------
+None
+"""
 def setMarkerPos(x = markerPos[0],y = markerPos[1]):
     markerPos[0] = x % 3
     markerPos[1] = y % 3
 
 #Display
 
-#Refreshes all of the shift states based on the board and marker position and shifts them to their respective registers
+"""
+This function displays the current state of the board.
+
+Parameters:
+    showMarker: A boolean that determines whether or not the marker is displayed.
+
+Returns:
+    None
+"""
 def refreshDisplay(showMarker = True):
     #Clear the board if the game is over
     if not gameInProgress:
@@ -289,6 +429,15 @@ def refreshDisplay(showMarker = True):
     #Apply Shift States
     shiftOut(shiftStates)
     
+"""
+    Builds the shift states for the board.
+    
+    Parameters:
+        board (list): The board to build the shift states for.
+        
+    Returns:
+        shiftStates (list): The shift states for the board.
+"""
 def buildShiftStates(board):
     for i, row in enumerate(board): #Get all the rows of the board
         for j, position in enumerate(row):  #Get all items in the row
@@ -303,6 +452,16 @@ def buildShiftStates(board):
                 for k in range(3):
                     shiftStates[association[k][0]][association[k][1]] = False
 
+"""
+Builds a list of lists of boolean values representing the current state of the LEDs.
+
+Parameters:
+    board (list): A list of lists of ints representing the current state of the board.
+    keys (dict): A dictionary of the form {str(int): [bool, bool, bool]} representing the current state of the keys.
+
+Returns:
+    list: A list of lists of booleans representing the current state of the LEDs.
+"""
 def buildColorStates(board, keys):
     state = []
     for i, row in enumerate(shiftStates):
@@ -320,6 +479,15 @@ def buildColorStates(board, keys):
 
                     
 #INPUTS
+"""
+This function processes the input from the controller and makes changes to the board based on that input.
+
+Parameters:
+    None
+
+Returns:
+    None
+"""
 def processInput():
     showMarker = True
     try:
@@ -398,7 +566,18 @@ def processInput():
         #Refresh the display after an input
         refreshDisplay(showMarker)
 
-#A menu for players to select their color
+"""
+    This function allows a player to select a color. 
+    It takes a playerID and the current color as parameters. 
+    It then asks the player to select a color and then changes the color of the player's pieces.
+
+Parameters:
+    playerID: the player who is selecting a color
+    currcolor: the current color of the player's pieces
+
+Returns:
+    None
+"""
 def selectColor(playerID):
     colorSelected = False
     currcolor = playerColors[0 if playerID == PLAYER1_NUM else 1]
@@ -447,7 +626,15 @@ def selectColor(playerID):
 
 #UTIL
 
-#Shifts out a boolean array to the shift registers
+"""
+This function takes a list of 8 booleans and shifts them out to the shift registers.
+
+Parameters:
+    states (list): A list of 8 booleans, each representing a state of the shift register.
+
+Returns:
+    None
+"""
 def shiftOut(states):
     clearRegisters()
     for i, state in enumerate(states): #Get all the registers
@@ -458,6 +645,18 @@ def shiftOut(states):
                 GPIO.output(clockPin, GPIO.HIGH) #Clock it
             GPIO.output(latchpins[i], GPIO.HIGH) #Latch it
 
+
+"""
+This function clears the registers and sets the outputs to low.
+
+Parameters:
+    dataPin: The GPIO pin that is connected to the data pin on the chip.
+    clockPin: The GPIO pin that is connected to the clock pin on the chip.
+    latchpins: The GPIO pins that are connected to the latch pins on the chip.
+
+Returns:
+    None
+"""
 def clearRegisters():
     GPIO.output(dataPin, GPIO.HIGH)
     for i in range(8):
@@ -470,14 +669,28 @@ def clearRegisters():
         GPIO.output(pin, GPIO.LOW)
 
 
-#Checks if 2 arrays have the same elements
+"""
+Returns True if A1 and A2 have different values at any index, False otherwise.
+
+Parameters:
+    A1 (list): A list of integers.
+    A2 (list): A list of integers.
+
+Returns:
+    bool: True if A1 and A2 have different values at any index, False otherwise.
+"""
 def matchingArrays(A1, A2):
     for i, thing in enumerate(A1):
         if A2[i] != thing:
             return True
     return False
 
-#Clones the current gameState
+"""
+Clones the game board.
+
+Returns:
+    list: A copy of the game state.
+"""
 def cloneBoard():
     newboard = []
     for i, thing in enumerate(gameState):
@@ -486,7 +699,30 @@ def cloneBoard():
             newboard[i].append(otherthing)
     return newboard
 
-#Checks if an array has all the same elements
+"""
+Returns True if all the elements in the list are the same, False otherwise.
+
+Parameters
+----------
+list : list
+    A list of any type of objects.
+
+Returns
+-------
+bool
+    True if all the elements in the list are the same, False otherwise.
+
+Examples
+--------
+>>> matchingRows([1, 1, 1, 1])
+True
+>>> matchingRows(['a', 'a', 'a', 'a'])
+True
+>>> matchingRows([1, 2, 3, 4])
+False
+>>> matchingRows(['a', 'b', 'c', 'd'])
+False
+"""
 def matchingRows(list):
     if(list is None or len(list) == 0):
         return False
@@ -496,6 +732,15 @@ def matchingRows(list):
             return False
     return True
 
+"""
+Returns True if the row is blocked by both players.
+
+Parameters:
+row (list): A list of numbers representing the row to check.
+
+Returns:
+bool: True if the row is blocked by both players, False otherwise.
+"""
 def isRowBlocked(row):
     return PLAYER1_NUM in row and PLAYER2_NUM in row
 
