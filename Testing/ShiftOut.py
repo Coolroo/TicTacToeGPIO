02811e2bd5f12d30d990d5f2840cf879abb4bb3f
Coolroo <coolrooswag@gmail.com>
2021-10-10 02:00:11 -0300

    new file:   Testing/ShiftOut.py

---
+++ Testing/ShiftOut.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+print("What is the latch pin?")
+latchPin = int(input())
+print("What is the Clock Pin?")
+clockPin = int(input())
+print("What is the data pin?")
+dataPin = int(input())
+
+print("What do you want to shift out?")
+data = input().split()
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(dataPin, GPIO.OUT)
+GPIO.setup(clockPin, GPIO.OUT)
+GPIO.setup(latchPin, GPIO.OUT)
+
+GPIO.output(latchPin, GPIO.LOW)
+for bit in data:
+    GPIO.output(dataPin, GPIO.HIGH if bit == "1" else GPIO.LOW)
+    GPIO.output(clockPin, GPIO.LOW)
+    GPIO.output(clockPin, GPIO.HIGH)
+GPIO.output(latchPin, GPIO.HIGH)
+
+input()
+GPIO.cleanup()
